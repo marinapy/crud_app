a7ffd3f8a97575001872d99ef14ac2dcff9f6cd2
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-07-19 18:50:40 -0400

    Checkpoint I, 1 and two

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,17 @@
-print("HELLO")
+menu = """
+
+Hi.
+
+Welcome to the prodcuts app.
+
+There are 100 products.
+
+Available operations: 'List', 'Show', 'Create', 'Update'
+
+Please choose an operation:
+
+"""
+
+chosen_operation = input(menu)
+
+print(chosen_operation)
