49421dfb8b85759a324757633108ea14078562f3
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-07-19 18:57:09 -0400

    Entire Checkpoint I

--- app/products_app.py
+++ app/products_app.py
@@ -14,4 +14,15 @@ Please choose an operation:
 
 chosen_operation = input(menu)
 
-print(chosen_operation)
+if chosen_operation.title() == "List":
+    print("LISTING PRODUCTS")
+elif chosen_operation.title() == "Show":
+    print("SHOWING A PRODUCT")
+elif chosen_operation.title() == "Create":
+    print("CREATING A PRODUCT")
+elif chosen_operation.title() == "Update":
+    print("UPDATING A PRODUCT")
+elif chosen_operation.title() == "Destroy":
+    print("DESTROYING A PRODUCT")
+else:
+    print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
