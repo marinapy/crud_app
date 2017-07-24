ec631b443fd35050e7c761a5cfa4699279700469
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-07-24 18:23:21 -0400

    All Check Points Complete

--- app/products_app.py
+++ app/products_app.py
@@ -1,39 +1,44 @@
 import csv
 
-
-# READ PRODCUTS CSV
+#READ PRODCUTS CSV
 
 products = []
 
 csv_file_path = "/Users/mtereo/Desktop/crud_app/data/product.csv"
+headers = ["id", "name", "aisle", "department", "price"]
+user_input_headers = [header for header in headers if header != "id"]
+
+def get_product_id(product): return int(product["id"])
+
+def auto_incremented_id():
+    product_ids = map(get_product_id, products)
+    return max(product_ids) + 1
 
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
-    for row in reader:
-        products.append(row)
+    for ordered_dict in reader:
+        products.append(dict(ordered_dict))
         #print(row["id"], row["name"])
 
-
-menu = """
-
-Hi.
-
-Welcome to the products app!
-
-There are 100 products.
-
-Available operations: 'List', 'Show', 'Create', 'Update'
-
-Please choose an operation:
-
-#""".format(len(products))
-
-csv_file_path = "/Users/mtereo/Desktop/crud_app/data/product.csv"
 with open(csv_file_path, "w") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
     writer.writeheader() # uses fieldnames set above
     for product in products:
         writer.writerow(product)
+
+menu = """
+
+Hello.
+
+Welcome to the products app!
+
+There are {20} products.
+
+Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
+
+Please choose an operation:
+
+"""
 
 chosen_operation = input(menu)
 chosen_operation = chosen_operation.title()
@@ -44,20 +49,44 @@ def list_products():
         print(" + Product #" + str(product["id"]) + ": " + product["name"])
 
 def show_products():
-    print("SHOWING A PRODUCT")
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("SHOWING PRODUCT", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product)
 
 def create_products():
-    print("CREATING A PRODUCT")
+    print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+    product = {"id": auto_incremented_id() }
+    for header in user_input_headers:
+        product[header] = input("The '{0}' is: ".format(header))
+    products.append(product)
+    print("CREATING PRODUCT", product)
 
 def update_products():
-    print("UPDATING A PRODUCT")
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+        for header in user_input_headers:
+            product[header] = input("Change '{0}' from '{1}' to: ".format(header, product[header]))
+        print("UPDATING PRODUCT HERE", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
 
 def destroy_products():
-    print("DESTROYING A PRODUCT")
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("DESTROYING PRODUCT HERE", product)
+        del products[products.index(product)]
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
 
 if chosen_operation.title() == "List": list_products()
 elif chosen_operation == "Show": show_products()
 elif chosen_operation == "Create": create_products()
-elif chosen_operation == "Update": update_products ()
-elif chosen_operation == "Destroy": destroy_products ()
+elif chosen_operation == "Update": update_products()
+elif chosen_operation == "Destroy": destroy_products()
 else: print("Unrecognized Operation. Please choose one of the following: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
